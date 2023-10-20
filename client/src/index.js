3cadfba2e99d4c1fdb1d5f7ef276f80daea9224a
Alberto Mallar <alberto.mallar@gmail.com>
2023-10-19 23:57:46 -0300

    comments

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( //aqui es donde vamos a inicializar el provider del store de REDUX
   <React.StrictMode>
     <App />
   </React.StrictMode>
