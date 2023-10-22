9a466030d0026da426b1063268a4519274f4a45b
Alberto Mallar <alberto.mallar@gmail.com>
2023-10-22 11:14:07 -0300

    feat(components and index): styles added to body index file and also started to create components such as Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar/Navbar.js";
 
 function App() { //en app se van a renderizar las paginas principales como home, navbar, etc.
   return (
     <div>
       <h1>Portfolio Home</h1>
+      <Navbar/>
     </div>
   );
 }
